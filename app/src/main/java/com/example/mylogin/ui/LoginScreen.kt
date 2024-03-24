ac56956244e1eb6df3203ebfb079769afb999e95
Omar <omarpk148@gmail.com>
2024-03-24 18:02:00 -0500

    mi fisrt commit

---
+++ app/src/main/java/com/example/mylogin/ui/LoginScreen.kt
@@ -0,0 +1,86 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.mylogin.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mylogin.R
+
+@Preview
+@Composable
+fun LoginScreen() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Login(Modifier.align(Alignment.Center))
+    }
+}
+
+@Composable
+fun Login(modifier: Modifier) {
+    Column(modifier = Modifier) {
+        HeaderImage(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.padding(16.dp))
+        EmailField()
+        Spacer(modifier = Modifier.padding(4.dp))
+        PasswordField()
+    }
+}
+
+@Composable
+fun PasswordField() {
+    TextField(
+        value = "", onValueChange = {},
+        placeholder = { Text(text = "Contraseña") },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+
+        )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailField() {
+    // Componente TextField para el correo electrónico
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = stringResource(id = R.string.email)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedTextColor = Color(0xFF636262),
+            unfocusedTextColor = Color(0xFFDEDDDD)
+        )
+    )
+}
+
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(painter = painterResource(id = R.drawable.tmx), contentDescription = "Header")
+}
